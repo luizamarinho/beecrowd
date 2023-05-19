cf8acaa40664dc05977c162c07f12fea1b229d7b
Luiza Marinho <luiza.calheirosm@gmail.com>
2023-05-19 16:17:38 -0300

    1146 e 1151

--- 1146.js
+++ 1146.js
@@ -1,24 +1,28 @@
-let input = '5\n10\n3\n0'
-let lines = input.split('\n')
+let input = '5\n10\n3\n0';
+let lines = input.split('\n');
 
-let indice = 0
-let encontrou = false
+let indice = 0;
+let encontrou = false;
 
-while(encontrou === false){
+while (encontrou === false) {
+  let entrada = parseFloat(lines[indice]);
 
-    let entrada = parseFloat(lines[indice])
+  if (entrada !== 0) {
+    let indice_2 = 0;
+    let sequencia = '';
 
-    console.log('funcionou')
-
-    let arquivo = 0
-
-    while(arquivo < entrada){
-        arquivo = arquivo + 1
-        console.log(arquivo)
+    while (indice_2 < entrada) {
+      sequencia = sequencia + (indice_2 + 1);
+      if (indice_2 + 1 !== entrada) {
+        sequencia = sequencia + ' ';
+      }
+      indice_2 = indice_2 + 1;
     }
 
-    if(entrada === 0){
-        encontrou = true
-    }
-    indice = indice + 1
+    console.log(sequencia);
+
+    indice = indice + 1;
+  } else {
+    encontrou = true;
+  }
 }
---
+++ 1151.js
@@ -0,0 +1,15 @@
+let input = '5';
+
+let entrada = parseFloat(input);
+
+let indice = 0;
+
+let valor = 0;
+let soma = '';
+let sequencia = '';
+
+while (indice < entrada) {
+  soma = valor + (valor + 1);
+  console.log(soma);
+  indice = indice + 1;
+}
